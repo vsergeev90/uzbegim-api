4aaa56d68e6a730cec57042b5d945a5cbd892b8a
vsergeev <ssf.svaleriy@gmail.com>
2021-01-24 14:24:30 +0100

    setting up CORS

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const http = require("http");
 const data = fs.readFileSync("./data.json", "utf-8");
 
 const server = http.createServer((req, res) => {
-  res.writeHead("200", { "Content-type": "application/json" });
+  res.writeHead("200", {
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, DELETE, HEAD, OPTIONS",
+  });
   res.end(data);
 });
 
