00ab6b99e04f1468f5a4e9b1516efa399ea8b1a1
vsergeev <ssf.svaleriy@gmail.com>
2021-02-08 00:13:05 +0100

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const menuRouter = require('./routes/menuRouter');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
